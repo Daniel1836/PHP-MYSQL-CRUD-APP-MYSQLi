3b814afd285d55600b9862f87dee6b649a0c58eb
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 07:14:39 -0400

    Update addstudtoprog.php

--- Create/addstudtoprog.php
+++ Create/addstudtoprog.php
@@ -1,34 +1,8 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-
-<title>
-Add a Student to a Program
-</title>
-
-<style>
-
-body {background-color: #75c7e2;}
-
-.form {border-style:solid;
-        border-radius: 25px;
-        border-color: black;
-        background-color: #59d259;
-        width: 500px;
-        padding: 20px;
-        position: absolute;
-        top: 40%;
-        left: 35%;
-        text-align: center;}
-
-</style>
-
-</head>
-
-<body>
-<div class="form">
-<h1> Add Students to Program </h1>
+ <body>
+  <div class="form">
+    <h1> Add Students to Program </h1>
 
 <form action="addsp.php" method="get">
 
@@ -40,14 +14,11 @@ body {background-color: #75c7e2;}
 
 					or die("There was a problem connecting");	
 
-			
-
+		
 			$sql = "select student_id, first_name, last_name from students";
 
 					
-
 			$result = mysqli_query($cox, $sql) or die("There was a problem searching");
-
 
 
 			while($row = mysqli_fetch_array($result)){
@@ -56,18 +27,11 @@ body {background-color: #75c7e2;}
 
 			}
 
-
-
 		?>
-
-	
 
 	</select>
 
 	
-
-
-
 	Program <select name="Name">				
 
 		<?php
@@ -77,13 +41,10 @@ body {background-color: #75c7e2;}
 					or die("There was a problem connecting");	
 
 			
-
 			$sql = "select program_id, name from programs";
 
 					
-
 			$result = mysqli_query($cox, $sql) or die("There was a problem searching");
-
 
 
 			while($row = mysqli_fetch_array($result)){
@@ -92,21 +53,16 @@ body {background-color: #75c7e2;}
 
 			}
 
-
-
 		?>
 
-	
-
 	</select>
+	
             <input type="submit" value="Submit">
 
-	
 
-</form>
-</div>
+    </form>
+   </div>
 
 
-</body>
-
+ </body>
 </html>
