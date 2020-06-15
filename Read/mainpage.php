01b09cdc97664fef0f4e0137730bcc4848956e14
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 07:36:07 -0400

    Update mainpage.php

--- Read/mainpage.php
+++ Read/mainpage.php
@@ -1,35 +1,10 @@
 <!DOCTYPE html>
-<html>
-
-<head>
-
-<title>
-PHP MYSQL Project
-</title>
-
-<style>
-body {background-color: #75c7e2;}
-
-.form {border-style:solid;
-        border-radius: 25px;
-        border-color: black;
-        background-color: #59d259;
-        width: 500px;
-        padding: 20px;
-        position: absolute;
-        top: 40%;
-        left: 31%;
-        text-align: center;}
-</style>
-
-
-
-
-</head>
-
-<body>
+ <html>
+  <body>
 <?php
+	  
 //echo $_SESSION['username'];
+	  
 if(!isset($_COOKIE['username'])){
 
 		header("Location: loginform.html");	
@@ -38,6 +13,7 @@ if(!isset($_COOKIE['username'])){
 
 	} 
 ?>
+	  
 <div class="form">
 <a href="studentmanagement.php">Student Management</a><br>
 <a href="programmanagement.php">Program Management</a><br>
@@ -46,6 +22,5 @@ if(!isset($_COOKIE['username'])){
 <br>
 <a href="logout.php">LogOut</a>
 </div>
-</body>
-
+ </body>
 </html>
