937c392bf6f71888fe1e5942fa836fbcf1017cd5
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 07:11:47 -0400

    Update logout.php

--- Authentication/logout.php
+++ Authentication/logout.php
@@ -1,13 +1,19 @@
 <?php
 	//Cleaning the session data
+
 	$_SESSION = array();
+
 	//Deleting the session ID
+
 setcookie('username','', time()-100);
-	if(isset($_COOKIES[session_name()])){
+	if(isset($_COOKIES[session_name()]))
+	{
 		setcookie(session_name(),'', time()-1);
 		
 	}
+
 	//Destroying the session
+
 	session_destroy();
 	header("Location: loginform.html");	
 	
